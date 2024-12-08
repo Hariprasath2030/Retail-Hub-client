f5a5f91aa0eb4a9c9e72983f9a44d8871056ba8f
Hariprasath2030 <webhari2005@gmail.com>
2024-12-08 16:39:28 +0530

    edit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://retail-hub-server.onrender.com',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '/data'),
             },
